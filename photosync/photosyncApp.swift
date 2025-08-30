40e20c67f0a38587cf3a385c5da8ced749afa9d8
Matthew Dolan <mdolancode@gmail.com>
2025-08-29 17:20:40 -0700

    Initial Commit

---
+++ photosync/photosyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  photosyncApp.swift
+//  photosync
+//
+//  Created by Matthew Dolan on 2025-08-29.
+//
+
+import SwiftUI
+
+@main
+struct photosyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
